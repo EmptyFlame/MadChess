19bae0cd955122e2fd5893b67dd532572884ec27
emptyflame <emptyflame.lh@gmail.com>
2011-06-08 13:50:06 +0000

    gameData was updated, and initialisation script was added.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
+//This function sets the board
 function setBoard() {
-    if(!allset){
+    if(!allSet){
         var piece;
         var assembler;
         for(piece in chessSet){
             assembler = $('<div id="'+piece+'" class="'+chessSet[piece].role+' '+chessSet[piece].team+'"></div>');
+            movePiece(assembler, chessSet[piece].position.x, chessSet[piece].position.y);
         }
-        allset = true;
+        allSet = true;
     }
 }
-
+//This function will moove the chess piece to desired coordinates (in the coordinate system of a chess board - #chessBoard)
 function movePiece(victim, x, y){
     var target = $('#chessBoard tr:nth-child('+y+') td:nth-child('+x+')');
     victim.appendTo(target);
